d95e7c3453caf8a5f885c005dbc78ef802bada55
snanoh <njs1324@gmail.com>
2022-05-10 20:29:19 +0900

    05.05  - proxyPattern 테스트 코드 추가

---
+++ src/test/java/hello/proxy/pureproxy/ProxyPatternTest.java
@@ -0,0 +1,29 @@
+package hello.proxy.pureproxy;
+
+import hello.proxy.pureproxy.code.CacheProxy;
+import hello.proxy.pureproxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
---
+++ src/test/java/hello/proxy/pureproxy/code/CacheProxy.java
@@ -0,0 +1,22 @@
+package hello.proxy.pureproxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CacheProxy implements Subject{
+    private Subject subject;
+    private String cacheValue;
+
+    public CacheProxy(Subject subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public String operation() {
+        log.info("프록시 호출");
+        if (cacheValue == null) {
+            cacheValue = subject.operation();
+        }
+        return cacheValue;
+    }
+}
